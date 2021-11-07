7ee2b481509f79effb3d067ca01144d123af18cf
Egor0603 <hardgames123@mail.ru>
2021-11-08 02:41:42 +0300

    Fixed secret key

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#!1e!1ji5mx$e7qa!ow!0py+aad2_$xchagbsdb&o7*m6tzx5'
+SECRET_KEY = 'секрентный ключ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
